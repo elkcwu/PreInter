6060f94c0f39d0ae3d8ffabf452c0f74bb46670f
Kevin Wu <elkcwu@gmail.com>
2017-05-23 15:06:17 -0700

    hange gitignore

--- src/com/kwu/arrays/reverseArray.java
+++ src/com/kwu/arrays/reverseArray.java
@@ -1,5 +1,22 @@
 package com.kwu.arrays;
 
 public class reverseArray {
-
+	public static void main(String[]args){
+		int [] arr1 = {12, 10, 9, 7, 5, 3};
+		
+		String reverseArr = reverseTheArray(arr1);
+		
+	}
+	//Reverse an array in-place, without using an additional buffer.
+	public static String reverseTheArray(int[] arr1){
+		if(arr1.length<2)
+			return arr1.toString();
+		
+		for(int i = 0; i < arr1.length - 1; i++){
+			
+			
+		}
+		
+		return new String();
+	}
 }
