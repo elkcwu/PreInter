84d5b846b224e961e3666df343248def3d733f2f
Kevin Wu <elkcwu@gmail.com>
2017-05-25 21:41:27 -0700

    find missing numbers in array

---
+++ src/com/kwu/arrays/MissingNumbers.java
@@ -0,0 +1,22 @@
+package com.kwu.arrays;
+
+import java.util.BitSet;
+
+public class MissingNumbers {
+	
+	public static void main(String[]args){
+		 
+		findingMissingNumbersz(new int[] {1,3,4,5,7,8,9,10}, 10);
+		findOneMissingNumber(new int[] {1,2,3,4,5,6,8,9,10});
+	}
+	
+	
+	public static void findingMissingNumbers(int[] ints, int count){
+		
+	}
+	
+	public static void findOneMissingNumber(int[] intArr){
+		
+	}
+
+}
