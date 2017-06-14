37a55a5fe88c586c2656f453f358ba2b6929009d
Kevin Wu <elkcwu@gmail.com>
2017-06-14 15:23:45 -0700

    add missing Number

--- src/com/kwu/arrays/MissingNumbers.java
+++ src/com/kwu/arrays/MissingNumbers.java
@@ -1,22 +1,43 @@
 package com.kwu.arrays;
 
+import java.util.Arrays;
 import java.util.BitSet;
 
 public class MissingNumbers {
 	
 	public static void main(String[]args){
 		 
-		findingMissingNumbersz(new int[] {1,3,4,5,7,8,9,10}, 10);
-		findOneMissingNumber(new int[] {1,2,3,4,5,6,8,9,10});
+		findingMissingNumbers(new int[] {1,2,4,5,6,8,9,10}, 10);
+		findOneMissingNumber(new int[] {1,2,3,4,5,6,8,9,10}, 10);
 	}
 	
-	
+	//this one can find any missing numbers
 	public static void findingMissingNumbers(int[] ints, int count){
+		int missingCount = count - ints.length;
+		BitSet bs = new BitSet();
+		for(int number : ints){
+			bs.set(number - 1);
+		}
 		
+		System.out.printf("Missing numbers in integer array %s, with total number %n", Arrays.toString(ints), count);
+		
+		int lastMissingIndex = 0;
+		for(int i=0; i<missingCount; i++){
+			lastMissingIndex = bs.nextClearBit(lastMissingIndex);
+			System.out.println(++lastMissingIndex);
+		}
 	}
 	
-	public static void findOneMissingNumber(int[] intArr){
+	
+	//this one can only find one missing number
+	public static void findOneMissingNumber(int[] intArr, int totalCount){
+		int expectedSum =  totalCount * (totalCount +1)/2;
+		int actualSum = 0;
+		for(int i : intArr){
+			actualSum += i;
+		}
 		
+		System.out.printf("the missing number is: %n" + (expectedSum - actualSum));
 	}
 
 }
