a36812693bcbd5d69fc9d7b1efc6b144d9585f96
Kevin Wu <elkcwu@gmail.com>
2017-07-27 22:06:00 -0700

    find the first and last

--- Arrays/com/kwu/arrays/FirstAndLastIndexOccur.java
+++ Arrays/com/kwu/arrays/FirstAndLastIndexOccur.java
@@ -4,22 +4,24 @@ public class FirstAndLastIndexOccur {
 	
 	public static void findFirstAndLastIndex(int[]arrInt, int target){
 		int first = -1, last = -1;
-		int index = 0;
-		for(int i : arrInt){
-			if(i == target){
+		int n = arrInt.length;
+		for(int i= 0; i<n; i++){
+			if(arrInt[i] == target){
 				if(first == -1){
-					first = index;
+					first = i;
 				}else{
-					last = index;
+					last = i;
 				}
-			}
-			index++;			
-			
-		}		
+			}				
+		}	
+		if(first != -1)
+			System.out.println("the first index is: " + first + " and the last index is: " + last);
+		else
+			System.out.println("none is found");
 	}
 	
 	public static void main(String[]args){
-		int[] arrInt = new int[] {1,3,5,7,4,9,7,8,7,5,10};
+		int[] arrInt = new int[] {17,3,5,7,4,9,7,8,7,5,10};
 		int target = 7;
 		findFirstAndLastIndex(arrInt, target);
 		
